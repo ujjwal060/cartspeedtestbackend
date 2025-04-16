b8082c9800dbb8c1a00027b61365565883226dab
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-04-16 13:15:27 +0530

    add change video get all sorting searching pagination

--- adminManagement/controllers/videosController.js
+++ adminManagement/controllers/videosController.js
@@ -6,7 +6,7 @@ const addVideos = async (req, res) => {
     try {
         const { title, description, locationState } = req.body;
         const uploadedBy = req.user.id;
-        const url=req.fileLocations[0];
+        const url = req.fileLocations[0];
 
         if (!title || !url || !locationState || !uploadedBy) {
             return res.status(400).json({
@@ -32,7 +32,7 @@ const addVideos = async (req, res) => {
         });
 
     } catch (error) {
-        logger.error(`addVideos Error`, error.message );
+        logger.error(`addVideos Error`, error.message);
         return res.status(500).json({
             status: 500,
             message: [error.message],
@@ -40,17 +40,116 @@ const addVideos = async (req, res) => {
     }
 }
 
-const getAllVideos=async(req,res)=>{
-    try{
+const getAllVideos = async (req, res) => {
+    try {
+        const { filters,sortField,sortBy,offset,limit } = req.body;
+        const parsedOffset = parseInt(offset);
+        const parsedLimit = parseInt(limit);
+        let aggregation = [];
 
-    }catch(error){
-        logger.error(`getAllVideos Error`, error.message );
+        if (filters?.title) {
+            aggregation.push({
+                $match: {
+                    title: {
+                        $regex: filters.title,
+                        $options: 'i'
+                    }
+                }
+            })
+        };
+
+        if (filters?.locationState) {
+            aggregation.push({
+                $match: {
+                    locationState: {
+                        $regex: filters.locationState,
+                        $options: 'i'
+                    }
+                }
+            })
+        };
+
+        if (filters?.views) {
+            aggregation.push({
+                $match: {
+                    views:filters?.views
+                }
+            })
+        };
+
+        aggregation.push({
+            $lookup: {
+                from: 'admins',
+                localField: 'uploadedBy',
+                foreignField: '_id',
+                as: 'uploadedBy'
+            }
+        });
+        aggregation.push({ $unwind: '$uploadedBy' });
+        aggregation.push({
+            $project: {
+                title: 1,
+                description: 1,
+                locationState: 1,
+                url:1,
+                views:1,
+                uploadedBy: {
+                    name: 1,
+                    role: 1
+                },
+                uploadDate:1
+            }
+        });
+
+        aggregation.push({
+            $sort: {
+                [sortField]: parseInt(sortBy) === 1 ? 1 : -1
+            }
+        });
+
+        // aggregation.push({ $skip: parsedOffset });
+        // aggregation.push({ $limit: parsedLimit });
+
+        aggregation.push({
+            $facet: {
+                data: [
+                    { $sort: { [sortField]: parseInt(sortBy) === 1 ? 1 : -1 } },
+                    { $skip: parsedOffset },
+                    { $limit: parsedLimit }
+                ],
+                totalCount: [
+                    { $count: 'count' }
+                ]
+            }
+        })
+
+        // const result = await videoModel.aggregate(aggregation);
+        const [result] = await videoModel.aggregate(aggregation);
+
+        const total = result.totalCount[0]?.count || 0;
+
+        return res.status(200).json({
+            status: 200,
+            message: ['Videos fetched successfully.'],
+            data: result.data,
+            total
+        });
+
+        return res.status(200).json({
+            status: 200,
+            message: ['Videos fetched successfully.'],
+            data: result
+        });
+
+    } catch (error) {
+        logger.error(`getAllVideos Error`, error.message);
         return res.status(500).json({
             status: 500,
             message: [error.message],
         });
     }
 }
+
 export {
     addVideos,
     getAllVideos
--- adminManagement/routes/videosRoutes.js
+++ adminManagement/routes/videosRoutes.js
@@ -2,12 +2,14 @@ import express from 'express';
 import {verifyTokenMiddleware}from "../adminmiddleware/adminAuthmiddleware.js"
 import {uploadToS3} from "../../utils/awsS3api.js"
 import {
-    addVideos
+    addVideos,
+    getAllVideos
 }from "../controllers/videosController.js";
 
 
 const router = express.Router();
 
 router.post('/add',uploadToS3,addVideos);
+router.post('/getAll',getAllVideos);
 
 export default router;
