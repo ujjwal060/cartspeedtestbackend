715592e990f9f30020d1cb8d03534153d4bf971e
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-05-07 12:08:03 +0530

    add change

--- usersManagement/controllers/userController.js
+++ usersManagement/controllers/userController.js
@@ -200,6 +200,7 @@ const loginUser = async (req, res) => {
             data: {
                 accessToken,
                 refreshToken,
+                userId:user._id
             }
         });
 
