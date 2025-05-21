ce9417cb91293f2c0d82dbee2bd7e95be1e4a96c
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-05-21 15:23:14 +0530

    add change

---
+++ adminManagement/controllers/goodLSVRuleController.js
@@ -0,0 +1,47 @@
+import goodLSVRulesModel from '../../models/goodLSVRulesModel.js';
+import adminModel from "../../models/adminModel.js";
+import { logger } from "../../utils/logger.js";
+
+const createLSVRule = async (req, res) => {
+    try {
+        const adminId = req.user.id;
+        const { questions, sections } = req.body;
+
+        if (!adminId || !questions || !sections) {
+            return res.status(400).json({ message: 'All required fields must be provided.' });
+        }
+
+        const location = await adminModel.findById(adminId);
+        if (!location) {
+            return res.status(404).json({
+                status: 404,
+                message: ['No location found for this admin.'],
+            });
+        }
+        const locationId = location.location;
+
+        const newRule = new goodLSVRulesModel({
+            locationId,
+            adminId,
+            questions,
+            sections
+        });
+        const savedRule = await newRule.save();
+
+        res.status(201).json({
+            status:201,
+            message: 'LSV Rule created successfully.',
+            data: savedRule
+        });
+    } catch (error) {
+        logger.error(`admin-createGLSV Error`, error.message);
+        return res.status(500).json({
+            status: 500,
+            message: [error.message],
+        });
+    }
+}
+
+export{
+    createLSVRule
+}
