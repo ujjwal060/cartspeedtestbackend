8ce6f099a337ecda64c4f2fa0671557eeb6f142d
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-05-22 15:46:57 +0530

    add chnage

--- adminManagement/controllers/videosController.js
+++ adminManagement/controllers/videosController.js
@@ -352,6 +352,7 @@ const addSafityVideos = async (req, res) => {
             title,
             url,
             locationId,
+            adminId,
             description,
             durationTime,
             isActive: true,
