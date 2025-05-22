2f86aec9fe36cd6cca829743407d798711f169d5
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-05-22 15:07:35 +0530

    add changee

--- adminManagement/routes/LSVRuleRoutes.js
+++ adminManagement/routes/LSVRuleRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import {
     createLSVRule,
-    getGLSVRules
+    getGLSVRules,
+    getRRLSVRules,
+    createRRLSV
 } from '../controllers/goodLSVRuleController.js';
 const router = express.Router();
 
 router.post('/addGLSVR', createLSVRule);
 router.get('/getGLSV', getGLSVRules);
+router.post('/addRRLSVR', createRRLSV);
+router.get('/getRRLSV', getRRLSVRules);
 
 export default router;
--- usersManagement/routes/lsvRuleRoutes.js
+++ usersManagement/routes/lsvRuleRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {
-getGLSVRule
+getGLSVRule,getRRLSVRule
 }from "../controllers/userLSVRulesController.js"
 const router = express.Router();
 
 router.get('/getGLSV',getGLSVRule);
+router.get('/getRRLSV',getRRLSVRule);
+
 
 export default router;
