e91746d8dd9506393af862b41d6a24b1bf1d3b6f
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-05-27 11:32:01 +0530

    ad dchnage

--- usersManagement/controllers/userVideoController.js
+++ usersManagement/controllers/userVideoController.js
@@ -126,7 +126,10 @@ const updateVideoProgress = async (req, res) => {
             return res.status(404).json({ status: 404, message: ['Video not found'] });
         }
 
-        const isCompleted = watchedDuration === video.durationTime;
+        const minutes = Math.floor(watchedDuration / 60);
+        const seconds = watchedDuration % 60;
+        const formattedDuration = `${minutes}m ${seconds}s`;
+        const isCompleted = formattedDuration === video.durationTime;
 
         let userProgress = await UserVideoProgress.findOne({ userId, locationId });
 
