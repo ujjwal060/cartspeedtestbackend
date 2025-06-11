be4facf793bafdfd48ac42a9ddd1868f2171d790
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-06-11 11:00:28 +0530

    add change

--- usersManagement/controllers/userLocationController.js
+++ usersManagement/controllers/userLocationController.js
@@ -1,4 +1,5 @@
 import userLocation from '../../models/userLocationMap.js';
+import locationModel from '../../models/locationModel.js';
 import { logger } from '../../utils/logger.js';
 
 const EARTH_RADIUS_MI = 3963.2;
@@ -16,7 +17,7 @@ const addOrUpdateUserLocation = async (req, res) => {
             return res.status(400).json({ message: 'All fields are required' });
         }
 
-        const coordinates = [latitude,longitude];
+        const coordinates = [latitude, longitude];
         const maxDistanceInMeters = MAX_DISTANCE_MI * METERS_PER_MILE;
 
         logger.info(`Checking nearby location for user: ${userId}`);
@@ -73,6 +74,51 @@ const addOrUpdateUserLocation = async (req, res) => {
     }
 };
 
+const gateLocationGeofence = async (req, res) => {
+    try {
+        const userId = req.user.userId;
+        const userLoc = await userLocation.findOne({ userId, isCurrent: true });
+        if (!userLoc || !userLoc.coordinates) {
+            return res.status(404).json({
+                status: 404,
+                message: ['User location not found'],
+            });
+        }
+        const userCoords = userLoc.coordinates.coordinates;
+
+        const matchedLocation = await locationModel.findOne({
+            geometry: {
+                $geoIntersects: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: userCoords,
+                    },
+                },
+            },
+        });
+
+        if (!matchedLocation) {
+            return res.status(404).json({
+                status: 404,
+                message: ['No matching location found near user coordinates'],
+            });
+        }
+
+        return res.status(200).json({
+            status: 200,
+            message: ['Location found'],
+            data: matchedLocation,
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: [error.message],
+        });
+    }
+}
+
 export {
     addOrUpdateUserLocation,
+    gateLocationGeofence
 }
--- usersManagement/routes/userLocationRoutes.js
+++ usersManagement/routes/userLocationRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {addOrUpdateUserLocation} from '../controllers/userLocationController.js';
+import {addOrUpdateUserLocation,gateLocationGeofence} from '../controllers/userLocationController.js';
 
 const router = express.Router();
 
 router.post('/addLocation', addOrUpdateUserLocation);
+router.get('/getGeofence', gateLocationGeofence);
 
 export default router;
