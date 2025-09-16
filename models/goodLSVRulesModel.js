ed461c1a848763ce898ba7f87c186433bcce48cb
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-09-16 19:17:33 +0530

    add chnage

--- models/goodLSVRulesModel.js
+++ models/goodLSVRulesModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const guidelineSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  imageUrl: { type: String, required: false, default: null },
 });
 
 const sectionSchema = new mongoose.Schema({
